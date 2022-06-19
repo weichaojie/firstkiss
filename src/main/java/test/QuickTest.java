58770757b70adda9d4fe640be3516e50680aeefd
15651875029 <404494241@qq.com>
2022-06-19 23:56:32 +0800

    add new features

--- src/main/java/test/QuickTest.java
+++ src/main/java/test/QuickTest.java
@@ -9,22 +9,66 @@ import java.util.*;
 public class QuickTest {
 
     public static void main(String[] args) {
+        QuickTest quickTest = new QuickTest();
 //        int[] nums={9,3,7,5,8,2,1,6,4,0};
 //        int[] sortedNums=SortUtil.quickSort(nums);
 //        System.out.println(Arrays.toString(sortedNums));
 //        System.out.println(SearchUtil.binerySearch(sortedNums, 3));
 
-        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        QuickTest quickTest = new QuickTest();
-        System.out.println(quickTest.removeDuplicates(nums));
+//        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+//        System.out.println(quickTest.removeDuplicates(nums));
+//
+//        ListNode head = new ListNode(3);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(0);
+//        head.next.next.next = new ListNode(-4);
+//        head.next.next.next.next = head.next;
 
-        ListNode head = new ListNode(3);
-        head.next = new ListNode(2);
-        head.next.next=new ListNode(0);
-        head.next.next.next=new ListNode(-4);
-        head.next.next.next.next=head.next;
-        quickTest.detectCycle(head);
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = head;
 
+//        quickTest.detectCycle(head);
+
+//        String s = "cars";
+//        List<String> wordDict = new ArrayList<>();
+//        wordDict.add("car");
+//        wordDict.add("ca");
+//        wordDict.add("rs");
+//        System.out.println(quickTest.wordBreak(s, wordDict));
+    }
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Collections.sort(wordDict);
+        StringBuilder sb = new StringBuilder(s);
+        List<String> strings = new ArrayList<>();
+
+        for (String word : wordDict) {
+            while (sb.indexOf(word) != -1) {
+                int index = sb.indexOf(word);
+                if (index != 0 && index != -1) {
+                    String[] strs = sb.toString().split(word);
+                    for (String str : strs) {
+                        strings.add(str);
+                    }
+                    sb = sb.delete(index, index + word.length());
+                } else if (index == 0) {
+                    sb = sb.delete(index, word.length());
+                }
+            }
+        }
+        if (sb.length() != 0) {
+            return false;
+        }
+
+        for (String str : strings) {
+            if (!wordBreak(str, wordDict)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public int removeDuplicates(int[] nums) {
@@ -42,19 +86,6 @@ public class QuickTest {
         }
 
         return temp.length;
-    }
-
-    public ListNode detectCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        while (head != null) {
-            if (set.contains(head)) {
-                break;
-            } else {
-                set.add(head);
-            }
-            head = head.next;
-        }
-        return head;
     }
 
 }
